ad50ecd441022d0cb463f991df51e2f991520246
hmiao <739025250@qq.com>
2021-11-23 20:34:49 +0800

    [feature] add identity contract

---
+++ src/impl/chain/contract.ts
@@ -0,0 +1,189 @@
+import common from "@ethereumjs/common";
+import { TransactionFactory } from "@ethereumjs/tx";
+import * as fs from "fs/promises";
+import log from "src/log";
+import { EventEmitter, PassThrough, Readable } from "stream";
+import Web3 from "web3";
+import { Log, provider, TransactionReceipt } from "web3-core";
+import { Contract, EventData } from "web3-eth-contract";
+import { AbiItem, sha3 } from "web3-utils";
+
+interface Abi extends AbiItem {
+  signature?: string;
+}
+
+interface ContractOption {
+  contractAddress: string;
+  nodeAddress: string;
+  privateKey: string;
+  provider: string;
+  abiFile: string;
+  gasPrice: number;
+  gasLimit: number;
+  chainParam: {
+    name?: string;
+    chainId?: number;
+  };
+}
+
+interface gasOption {
+  gasPrice?: number;
+  gasLimit?: number;
+}
+
+type Result = { [key: string]: string };
+type Mixed = { [key: string | number]: string | any } | string;
+
+export class ContractHelper {
+  private web3!: Web3;
+  private abis!: Abi[];
+  private contract!: Contract;
+
+  private option: ContractOption;
+
+  private unsubscribeMap: Map<Readable, () => void> = new Map();
+
+  constructor(option: ContractOption) {
+    this.option = option;
+  }
+
+  async init(): Promise<void> {
+    const provider = await this.connect(this.option.provider);
+    this.web3 = new Web3(provider);
+
+    const content = await fs.readFile(this.option.abiFile, { encoding: "utf-8" });
+    const jsonInterface = JSON.parse(content);
+    const rawAbis: AbiItem[] = jsonInterface.abi;
+    this.abis = rawAbis.map((abi) => {
+      if (abi.type === "event") {
+        const signature = abi.name + "(" + abi.inputs!.map((input) => input.type).join(",") + ")";
+        const hash = sha3(signature);
+        return { ...abi, signature: hash || undefined };
+      } else {
+        return abi;
+      }
+    });
+
+    this.contract = new this.web3.eth.Contract(this.abis, this.option.contractAddress);
+  }
+
+  async connect(url: string): Promise<provider> {
+    const provider = new Web3.providers.WebsocketProvider(url, {
+      reconnect: {
+        auto: true,
+        delay: 5000,
+        maxAttempts: 5,
+        onTimeout: false,
+      },
+    });
+    return new Promise((resolve, reject) => {
+      provider.on("connect", () => {
+        log.info("Blockchain connected");
+        resolve(provider);
+      });
+      provider.on("error", () => {
+        reject();
+      });
+    });
+  }
+
+  async method(
+    name: string,
+    args: any[] = [],
+    nonce: number = 0,
+    gasOpt: gasOption | null = null
+  ): Promise<TransactionReceipt> {
+    const method = this.contract.methods[name](...args);
+    const data = method.encodeABI();
+
+    const gasPrice = gasOpt?.gasPrice || this.option.gasPrice;
+    const gasLimit = gasOpt?.gasLimit || this.option.gasLimit;
+
+    if (nonce === 0) {
+      nonce = await this.web3.eth.getTransactionCount(this.option.nodeAddress);
+    }
+
+    const tra = {
+      data: data,
+      from: this.option.nodeAddress,
+      to: this.option.contractAddress,
+      gasPrice: "0x" + gasPrice.toString(16),
+      gasLimit: "0x" + gasLimit.toString(16),
+      nonce: this.web3.utils.toHex(nonce),
+    };
+
+    let privateKey = this.option.privateKey;
+    if (privateKey.startsWith("0x")) {
+      privateKey = privateKey.slice(2);
+    }
+    const key = Buffer.from(privateKey, "hex");
+    const tx = TransactionFactory.fromTxData(tra, {
+      common: common.custom(this.option.chainParam),
+    });
+    const serializedTx = "0x" + tx.sign(key).serialize().toString("hex");
+    return new Promise((resolve, reject) => {
+      this.web3.eth
+        .sendSignedTransaction(serializedTx, (err, txHash) => {
+          if (err) {
+            log.error(err);
+            reject(err);
+          } else {
+            log.info(`method ${name} tx hash ${txHash}`);
+          }
+        })
+        .then((receipt) => {
+          if (receipt.status) {
+            resolve(receipt);
+          } else {
+            const err = `method ${name} tx hash ${receipt.transactionHash} is reverted`;
+            reject(err);
+          }
+        });
+    });
+  }
+
+  decodeLogs(logs: Log[]): Result | undefined {
+    for (const rlog of logs) {
+      const topics = rlog.topics;
+      const data = rlog.data;
+      for (const abi of this.abis) {
+        if (abi.signature && topics.includes(abi.signature) && abi.inputs) {
+          const result = this.web3.eth.abi.decodeLog(abi.inputs, data, topics.slice(1));
+          result.name = abi.name ? abi.name : result.name;
+          return result;
+        }
+      }
+    }
+    return undefined;
+  }
+
+  async call(name: string, args: any[] = []): Promise<Mixed> {
+    const method = this.contract.methods[name](...args);
+    const res = await method.call({ from: this.option.nodeAddress });
+    return res;
+  }
+
+  subscribe(): Readable {
+    const stream = new PassThrough({ objectMode: true });
+    const callback = (event: EventData) => {
+      stream.write(event);
+    };
+
+    const emitter: EventEmitter = this.contract.events.allEvents({ fromBlock: "latest" });
+    emitter.on("data", callback);
+
+    const unsubscribe = () => {
+      emitter.off("data", callback);
+    };
+    this.unsubscribeMap.set(stream, unsubscribe);
+
+    return stream;
+  }
+
+  unsubscribe(stream: Readable): void {
+    const unsubscribe = this.unsubscribeMap.get(stream);
+    if (unsubscribe) {
+      unsubscribe();
+    }
+  }
+}
--- src/impl/chain/service.test.ts
+++ src/impl/chain/service.test.ts
@@ -1,6 +1,6 @@
 import * as chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { impl, ImplOption } from ".";
+import { impl, ChainOption, ContractOption } from ".";
 import * as crypto from "crypto";
 import {
   Event,
@@ -18,10 +18,23 @@ chai.use(chaiAsPromised);
 const assert = chai.assert;
 
 describe("chain service", function () {
-  const opt: ImplOption = {
-    contractAddress: "0xA7E337B441190057D8974c47f93A929aB302dA91",
-    nodeAddress: "0x29E369a0eCdB439d3e42C955134ff4DF1894b32b",
-    privateKey: "0x90a7f38ec7d0c2ab9b1302d0d99b485dc31e3dace206860258b5fe8f696b21ad",
+  const identityOpt: ContractOption = {
+    contractAddress: "0x8665E1047287352f958dd926391752694D416519",
+    nodeAddress: "0xA3537C2586322A4B443327B2b2e1505fdCa8EADE",
+    privateKey: "0x9ef421a93c8b6c01145147bfbef7444ed6f5b2fe5e99bdbbaaf5f6f21f238c7b",
+    provider: "http://127.0.0.1:8545",
+    abiFile: "IdentityContract.json",
+    gasPrice: 1,
+    gasLimit: 6721975,
+    chainParam: {
+      chainId: 1337,
+    },
+  };
+
+  const deltaOpt: ContractOption = {
+    contractAddress: "0x69AB89d460F8f0F39302D98cb5f71f412259E4D2",
+    nodeAddress: "0xA3537C2586322A4B443327B2b2e1505fdCa8EADE",
+    privateKey: "0x9ef421a93c8b6c01145147bfbef7444ed6f5b2fe5e99bdbbaaf5f6f21f238c7b",
     provider: "http://127.0.0.1:8545",
     abiFile: "DeltaContract.json",
     gasPrice: 1,
@@ -30,6 +43,13 @@ describe("chain service", function () {
       chainId: 1337,
     },
   };
+
+  const opt: ChainOption = {
+    identity: identityOpt,
+    delta: deltaOpt,
+  };
+
+  const nodeAddress = "0xA3537C2586322A4B443327B2b2e1505fdCa8EADE";
 
   let stream: Readable;
   const events: Event[] = [];
@@ -45,11 +65,24 @@ describe("chain service", function () {
     impl.unsubscribe(stream);
   });
 
+  const serverUrl = "127.0.0.1:6700";
+  const serverName = "first";
+  describe("node join", function () {
+    it("join and get", async function () {
+      const address = await impl.join(serverUrl, serverName);
+      assert.strictEqual(address, nodeAddress);
+
+      const info = await impl.getNodeInfo(address);
+      assert.strictEqual(info.url, serverUrl);
+      assert.strictEqual(info.name, serverName);
+    });
+  });
+
   let taskID: string;
   const taskCommitment = "0x" + crypto.randomBytes(32).toString("hex");
   describe("createTask", function () {
     it("create first task", async function () {
-      taskID = await impl.createTask(opt.nodeAddress, "mnist", taskCommitment);
+      taskID = await impl.createTask(nodeAddress, "mnist", taskCommitment);
       assert.strictEqual(taskID.slice(0, 2), "0x");
       assert.lengthOf(taskID, 66);
     });
@@ -58,7 +91,7 @@ describe("chain service", function () {
   const round = 1;
   describe("startRound", function () {
     it("start round 1", async function () {
-      await impl.startRound(opt.nodeAddress, taskID, round);
+      await impl.startRound(nodeAddress, taskID, round);
     });
   });
 
@@ -66,7 +99,7 @@ describe("chain service", function () {
   const pk2 = "0x" + crypto.randomBytes(32).toString("hex");
   describe("joinRound", function () {
     it("join round 1", async function () {
-      await impl.joinRound(opt.nodeAddress, taskID, round, pk1, pk2);
+      await impl.joinRound(nodeAddress, taskID, round, pk1, pk2);
     });
   });
 
@@ -74,7 +107,7 @@ describe("chain service", function () {
     it("getTaskRound 1", async function () {
       const info = await impl.getTaskRound(taskID, round);
       assert.strictEqual(info.round, 1);
-      assert.include(info.clients, opt.nodeAddress);
+      assert.include(info.clients, nodeAddress);
       assert.lengthOf(info.clients, 1);
       assert.strictEqual(info.status, 0);
     });
@@ -82,10 +115,10 @@ describe("chain service", function () {
 
   describe("selectCandidates", function () {
     it("selectCandidates", async function () {
-      await impl.selectCandidates(opt.nodeAddress, taskID, round, [opt.nodeAddress]);
+      await impl.selectCandidates(nodeAddress, taskID, round, [nodeAddress]);
       assert.isRejected(
-        impl.selectCandidates(opt.nodeAddress, taskID, round, [
-          opt.nodeAddress,
+        impl.selectCandidates(nodeAddress, taskID, round, [
+          nodeAddress,
           "0x8Caf33d112b4695f630c2E80E41Fc0336470b3b1",
         ])
       );
@@ -98,26 +131,20 @@ describe("chain service", function () {
   const seedCommitment = "0x" + crypto.randomBytes(32).toString("hex");
   describe("uploadSeedCommitment", function () {
     it("uploadSeedCommitment", async function () {
-      await impl.uploadSeedCommitment(opt.nodeAddress, taskID, round, opt.nodeAddress, seedCommitment);
+      await impl.uploadSeedCommitment(nodeAddress, taskID, round, nodeAddress, seedCommitment);
     });
   });
 
   const secretKeyCommitment = "0x" + crypto.randomBytes(32).toString("hex");
   describe("uploadSecretKeyCommitment", function () {
     it("uploadSecretKeyCommitment", async function () {
-      await impl.uploadSecretKeyCommitment(
-        opt.nodeAddress,
-        taskID,
-        round,
-        opt.nodeAddress,
-        secretKeyCommitment
-      );
+      await impl.uploadSecretKeyCommitment(nodeAddress, taskID, round, nodeAddress, secretKeyCommitment);
     });
   });
 
   describe("getClientPublickKeys", function () {
     it("getClientPublickKeys", async function () {
-      const [recvPK1, recvPK2] = await impl.getClientPublickKeys(taskID, round, opt.nodeAddress);
+      const [recvPK1, recvPK2] = await impl.getClientPublickKeys(taskID, round, nodeAddress);
       assert.strictEqual(recvPK1, pk1);
       assert.strictEqual(recvPK2, pk2);
     });
@@ -125,10 +152,10 @@ describe("chain service", function () {
 
   describe("startCalculation", function () {
     it("startCalculation", async function () {
-      await impl.startCalculation(opt.nodeAddress, taskID, round, [opt.nodeAddress]);
+      await impl.startCalculation(nodeAddress, taskID, round, [nodeAddress]);
       assert.isRejected(
-        impl.startCalculation(opt.nodeAddress, taskID, round, [
-          opt.nodeAddress,
+        impl.startCalculation(nodeAddress, taskID, round, [
+          nodeAddress,
           "0x8Caf33d112b4695f630c2E80E41Fc0336470b3b1",
         ])
       );
@@ -141,18 +168,18 @@ describe("chain service", function () {
   const resultCommitment = "0x" + crypto.randomBytes(32).toString("hex");
   describe("resultCommitment", function () {
     it("upload and get resultCommitment", async function () {
-      await impl.uploadResultCommitment(opt.nodeAddress, taskID, round, resultCommitment);
-      const cm = await impl.getResultCommitment(taskID, round, opt.nodeAddress);
+      await impl.uploadResultCommitment(nodeAddress, taskID, round, resultCommitment);
+      const cm = await impl.getResultCommitment(taskID, round, nodeAddress);
       assert.strictEqual(cm, resultCommitment);
     });
   });
 
   describe("startAggregation", function () {
     it("startAggregation", async function () {
-      await impl.startAggregation(opt.nodeAddress, taskID, round, [opt.nodeAddress]);
+      await impl.startAggregation(nodeAddress, taskID, round, [nodeAddress]);
       assert.isRejected(
-        impl.startAggregation(opt.nodeAddress, taskID, round, [
-          opt.nodeAddress,
+        impl.startAggregation(nodeAddress, taskID, round, [
+          nodeAddress,
           "0x8Caf33d112b4695f630c2E80E41Fc0336470b3b1",
         ])
       );
@@ -165,20 +192,20 @@ describe("chain service", function () {
   const seed = "0x" + crypto.randomBytes(32).toString("hex");
   describe("uploadSeed", function () {
     it("uploadSeed", async function () {
-      await impl.uploadSeed(opt.nodeAddress, taskID, round, opt.nodeAddress, seed);
+      await impl.uploadSeed(nodeAddress, taskID, round, nodeAddress, seed);
     });
   });
 
   const secretKey = "0x" + crypto.randomBytes(32).toString("hex");
   describe("uploadSecretKey", function () {
     it("uploadSecretKey", async function () {
-      await impl.uploadSecretKey(opt.nodeAddress, taskID, round, opt.nodeAddress, secretKey);
+      await impl.uploadSecretKey(nodeAddress, taskID, round, nodeAddress, secretKey);
     });
   });
 
   describe("getSecretShareData", function () {
     it("getSecretShareData", async function () {
-      const data = await impl.getSecretShareData(taskID, round, opt.nodeAddress, opt.nodeAddress);
+      const data = await impl.getSecretShareData(taskID, round, nodeAddress, nodeAddress);
       assert.strictEqual(data.seed, seed);
       assert.strictEqual(data.seedCommitment, seedCommitment);
       assert.strictEqual(data.secretKey, secretKey);
@@ -188,7 +215,7 @@ describe("chain service", function () {
 
   describe("endRound", function () {
     it("endRound", async function () {
-      await impl.endRound(opt.nodeAddress, taskID, round);
+      await impl.endRound(nodeAddress, taskID, round);
 
       const info = await impl.getTaskRound(taskID, round);
       assert.strictEqual(info.round, 1);
@@ -201,7 +228,7 @@ describe("chain service", function () {
       assert.lengthOf(events, 6);
       assert.strictEqual(events[0].type, "TaskCreated");
       const event0 = events[0] as TaskCreatedEvent;
-      assert.strictEqual(event0.address, opt.nodeAddress);
+      assert.strictEqual(event0.address, nodeAddress);
       assert.strictEqual(event0.taskID, taskID);
       assert.strictEqual(event0.dataset, "mnist");
       assert.strictEqual(event0.commitment, taskCommitment);
@@ -216,26 +243,41 @@ describe("chain service", function () {
       assert.strictEqual(event2.taskID, taskID);
       assert.strictEqual(event2.round, round);
       assert.lengthOf(event2.addrs, 1);
-      assert.include(event2.addrs, opt.nodeAddress);
+      assert.include(event2.addrs, nodeAddress);
 
       assert.strictEqual(events[3].type, "CalculationStarted");
       const event3 = events[3] as CalculationStartedEvent;
       assert.strictEqual(event3.taskID, taskID);
       assert.strictEqual(event3.round, round);
       assert.lengthOf(event3.addrs, 1);
-      assert.include(event3.addrs, opt.nodeAddress);
+      assert.include(event3.addrs, nodeAddress);
 
       assert.strictEqual(events[4].type, "AggregationStarted");
       const event4 = events[4] as AggregationStartedEvent;
       assert.strictEqual(event4.taskID, taskID);
       assert.strictEqual(event4.round, round);
       assert.lengthOf(event4.addrs, 1);
-      assert.include(event4.addrs, opt.nodeAddress);
+      assert.include(event4.addrs, nodeAddress);
 
       assert.strictEqual(events[5].type, "RoundEnded");
       const event5 = events[5] as RoundEndedEvent;
       assert.strictEqual(event5.taskID, taskID);
       assert.strictEqual(event5.round, round);
+    });
+  });
+
+  const newServerUrl = "127.0.0.1:6701";
+  const newServerName = "new";
+  describe("node update and leave", function () {
+    it("node update and leave", async function () {
+      await impl.updateUrl(nodeAddress, newServerUrl);
+      await impl.updateName(nodeAddress, newServerName);
+
+      const info = await impl.getNodeInfo(nodeAddress);
+      assert.strictEqual(info.url, newServerUrl);
+      assert.strictEqual(info.name, newServerName);
+
+      await impl.leave(nodeAddress);
     });
   });
 });
