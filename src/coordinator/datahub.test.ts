94b7d39800952644b19cd6dab386871f20d1fe99
hmiao <739025250@qq.com>
2022-09-19 10:33:45 +0800

    datahub coordinator test

---
+++ src/coordinator/datahub.test.ts
@@ -0,0 +1,49 @@
+import { Options } from "@mikro-orm/core";
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { datahub } from "./datahub";
+import { identity } from "./identity";
+
+chai.use(chaiAsPromised);
+
+const assert = chai.assert;
+
+describe("datahub coordinator", () => {
+  let address: string;
+
+  before(async () => {
+    const dbConfig: Options = {
+      type: "sqlite",
+      dbName: ":memory:",
+      entities: ["dist/entity/**/*.js"],
+      entitiesTs: ["src/entity/**/*.ts"],
+    };
+    await datahub.init(dbConfig);
+
+    address = (await identity.join("127.0.0.1:6700", "node1"))[1];
+  });
+
+  const dataset = "mnist";
+  const commitment = "0x1230000000000000000000000000000000000000000000000000000000000000";
+
+  it("register", async () => {
+    await datahub.register(address, dataset, commitment);
+
+    const _commitment = await datahub.getDataCommitment(address, dataset);
+    assert.strictEqual(_commitment, commitment);
+
+    const version = await datahub.getDataVersion(address, dataset);
+    assert.strictEqual(version, 1);
+  });
+
+  const newCommitment = "0x1230000000000000000000000000000000000000000000000000000000000001";
+  it("update", async () => {
+    await datahub.register(address, dataset, newCommitment);
+
+    const _commitment = await datahub.getDataCommitment(address, dataset);
+    assert.strictEqual(_commitment, newCommitment);
+
+    const version = await datahub.getDataVersion(address, dataset);
+    assert.strictEqual(version, 2);
+  });
+});
