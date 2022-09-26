2ea9f8c6950fd88668ee471e326dc29ad53d79f6
hmiao <739025250@qq.com>
2022-09-26 20:36:23 +0800

    bug fix: add subscribe service

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,10 +3,12 @@ import * as datahub from "./datahub";
 import * as hlr from "./hlr";
 import * as horizontal from "./horizontal";
 import * as identity from "./identity";
+import * as subscribe from "./subscribe";
 
 export function addService(server: Server): void {
   datahub.addService(server);
   identity.addService(server);
   horizontal.addService(server);
   hlr.addService(server);
+  subscribe.addService(server);
 }
